b9c0b89c00211b9b68faba4cba95ac355a18e4f2
Sai Datta Kunchala <saidatta1432015@gmail.com>
2024-12-09 21:47:55 +0530

    Remove value prop and add state to each square and set it on click event

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
+import { useState } from "react";
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={1}/>
-        <Square value={2}/>
-        <Square value={3}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={4}/>
-        <Square value={5}/>
-        <Square value={6}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={7}/>
-        <Square value={8}/>
-        <Square value={9}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
 );
 }
 
-export function Square({value}){
+export function Square(){
+
+  const[value, setValue] = useState(null);
 
   function handleClick(){
-    console.log("clicked!");
+   setValue('X');
   }
   return <button className="square" onClick={handleClick}>{value}</button>;
 }
