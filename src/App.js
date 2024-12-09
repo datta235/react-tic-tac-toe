a1a281e84bf4a6ed4c5fccaddcae2179b62df63e
Sai Datta Kunchala <saidatta1432015@gmail.com>
2024-12-09 21:21:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+    </>
+);
+}
+
+export function Square(){
+  return <button className="square">1</button>;
+}
